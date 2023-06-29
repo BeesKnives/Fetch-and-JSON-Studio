c13341f008cb4b41c83bc08a220b08caf51d7614
Anna <Anna@Annas-MacBook-Air.local>
2023-06-28 20:07:22 -0500

    adding more

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            console.log(json)
+            let astronautDesign = document.getElementById("container")
+            let i=0
+        
+            while (i< JSON.length){
+                
+                astronautDesign.innerHTML= 
+                `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li> 
+                        </ul>
+                    </div>
+                    <img class="avatar" src="images/${json[i].picture}">
+                </div>
+                `
+
+
+
+                i++
+            }
+
+
+
+
+        })
+
+     
+
+    })
+
+
+
+})
+console.log(getElementsByClassName("bio"))
